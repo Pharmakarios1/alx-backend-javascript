f43fb2f24a38b83e2c22b84a821c834bde4ff57b
Pharmakarios1 <akhigbeblessed@gmail.com>
2023-10-05 03:29:28 +0100

    task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,7 @@
+//1-main.js
+
+import getListStudentIds from "./1-get_list_student_ids.js";
+import getListStudents from "./0-get_list_students.js";
+
+console.log(getListStudentIds("hello")); // Output: []
+console.log(getListStudentIds(getListStudents())); // Output: [1, 2, 5]
